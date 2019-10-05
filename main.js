d45a1b5b17c26752b39d88430c3e2c5fb93c7623
Tousif <tousifislampersonal@outlook.com>
2019-10-05 13:57:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function getFactFetch() {
   // Using Fetch API
   let number = numberInput.value
 
-  fetch('https://numbersapi.com/'+ number + '/year')
+  fetch('http://numbersapi.com/'+ number + '/year')
     .then(response => response.text())
     .then(data => { 
       if (number != '') {
